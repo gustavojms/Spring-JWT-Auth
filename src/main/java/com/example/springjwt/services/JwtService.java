91e9f60ffbdcd28d18c6e1fee159939ee0c1b335
gustavojms <gustavo.joset0@hotmail.com>
2023-02-20 21:27:27 -0300

    Adding all extract methods of the jwt implementation, also adding generate token, is token expired or valid

--- src/main/java/com/example/springjwt/services/JwtService.java
+++ src/main/java/com/example/springjwt/services/JwtService.java
@@ -1,10 +1,69 @@
 package com.example.springjwt.services;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
-    public String getUserEmail(String jwtToken) {
-        return null;
+
+    private static final String SECRET_KEY = "";
+
+    public String extractUserEmail(String jwtToken) {
+        return extractClaim(jwtToken, Claims::getSubject);
     }
+
+    public <T> T extractClaim(String jwtToken, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(jwtToken);
+
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String jwtToken) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(jwtToken)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(Map<String, Object> extractClaims, UserDetails userDetails) {
+        return Jwts.builder()
+                .setClaims(extractClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private boolean isTokenExpired(String jwtToken) {
+        return extractExpiration(jwtToken).before(new Date());
+    }
+
+    private Date extractExpiration(String jwtToken) {
+        return extractClaim(jwtToken, Claims::getExpiration);
+    }
+
+    public boolean isTokenValid(String jwtToken, UserDetails userDetails) {
+        final String useremail = extractUserEmail(jwtToken);
+
+        return (useremail.equals(userDetails.getUsername())) && !isTokenExpired(jwtToken);
+    }
+
 }
