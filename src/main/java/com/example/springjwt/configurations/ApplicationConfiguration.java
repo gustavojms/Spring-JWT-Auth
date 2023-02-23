db62d065431b872aa1b03a7602cae99292619f48
gustavojms <gustavo.joset0@hotmail.com>
2023-02-22 21:24:11 -0300

    Renaming file to application configuration and adding security configuration file

--- src/main/java/com/example/springjwt/configurations/ApplicationConfiguration.java
+++ src/main/java/com/example/springjwt/configurations/ApplicationConfiguration.java
@@ -4,13 +4,12 @@ import com.example.springjwt.User.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @Configuration
 @RequiredArgsConstructor
-public class ApplicationConfig {
+public class ApplicationConfiguration {
 
     private final UserRepository userRepository;
 
